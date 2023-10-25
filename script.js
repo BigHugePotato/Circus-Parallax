99a684cd3ccee81d6a5e381853352332efad3d07
BigHugePotato <adrianb09.kodehode@gmail.com>
2023-10-25 20:31:56 +0200

    Finish top half, added pictures

--- script.js
+++ script.js
@@ -10,34 +10,38 @@ let initialTopCloud1 = cloud1.offsetTop;
 let initialLeftCloud1 = cloud1.offsetLeft;
 
 let initialTopCloud2 = cloud2.offsetTop;
+let initialLeftCloud2 = cloud2.offsetLeft;
+
 let initialTopCloud3 = cloud3.offsetTop;
+let initialLeftCloud3 = cloud3.offsetLeft;
+
 let initialTopCloud4 = cloud4.offsetTop;
+let initialLeftCloud4 = cloud4.offsetLeft;
+
+let initialTopCloud5 = cloud5.offsetTop;
 let initialLeftCloud5 = cloud5.offsetLeft;
 
 
 window.addEventListener('scroll', () => {
   let value = window.scrollY;
 
-  text.style.marginTop = value * 2.5 + 'px';
+  text.style.marginTop = value * 0.95 + 'px';
 
-  // For cloud1: Assuming it starts to the right of center and above center
-  cloud1.style.top = initialTopCloud1 - value * 0.9 + 'px'; // Move up
-  cloud1.style.left = initialLeftCloud1 + value * 0.9 + 'px'; // Move right
 
-  cloud2.style.top = value * -1.5 + 'px';
+  cloud1.style.top = initialTopCloud1 - value * 0.21 + 'px'; // Move up
+  cloud1.style.left = initialLeftCloud1 + value * -0.3 + 'px'; // Move right
 
-  // For cloud3: Adjust based on its initial position.
-  // This is just an example; adjust as per its actual position.
-  cloud3.style.top = initialTopCloud3 - value * 1.5 + 'px'; // Move up if it's above center
-  cloud3.style.left = initialLeftCloud3 - value * 1.5 + 'px'; // Move left if it's to the left of center
+  cloud2.style.top = initialTopCloud2 - value * 0.25 + 'px'; // Move up
+  cloud2.style.left = initialLeftCloud2 + value * 0.8 + 'px'; // Move right
 
-  // Do similar adjustments for cloud4 and cloud5. Use initial positions to determine the direction.
+  cloud3.style.top = initialTopCloud3 - value * 0.3 + 'px'; // Move up
+  cloud3.style.left = initialLeftCloud3 - value * 0.3 + 'px'; // Move right
 
-  cloud4.style.top = initialTopCloud4 - value * 2.5 + 'px'; // Example
-  cloud4.style.left = initialLeftCloud4 - value * 2.5 + 'px'; // Example
+  cloud4.style.top = initialTopCloud4 - value * 0.22 + 'px'; // Move up
+  cloud4.style.left = initialLeftCloud4 - value * -0.3 + 'px'; // Move right
 
-  cloud5.style.top = initialTopCloud5 + value * 1.5 + 'px'; // Example
-  cloud5.style.left = initialLeftCloud5 + value * 1.5 + 'px'; // Example
+  cloud5.style.top = initialTopCloud5 + value * -0.36 + 'px'; // Move up
+  cloud5.style.left = initialLeftCloud5 + value * -0.2 + 'px'; // Move right
 });
 
 
