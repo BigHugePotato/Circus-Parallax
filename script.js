46df2ae308c3c413e27eeb2dd813bc093c7c5156
adrianb09 <adrianb09.kodehode@gmail.com>
2023-10-27 12:43:11 +0200

    Final changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let text = document.getElementById('text');
+let text = document.getElementById("text");
 
-let cloud1 = document.getElementById('cloud1');
-let cloud2 = document.getElementById('cloud2');
-let cloud3 = document.getElementById('cloud3');
-let cloud4 = document.getElementById('cloud4');
-let cloud5 = document.getElementById('cloud5');
+let cloud1 = document.getElementById("cloud1");
+let cloud2 = document.getElementById("cloud2");
+let cloud3 = document.getElementById("cloud3");
+let cloud4 = document.getElementById("cloud4");
+let cloud5 = document.getElementById("cloud5");
 
 let initialTopCloud1 = cloud1.offsetTop;
 let initialLeftCloud1 = cloud1.offsetLeft;
@@ -21,27 +21,23 @@ let initialLeftCloud4 = cloud4.offsetLeft;
 let initialTopCloud5 = cloud5.offsetTop;
 let initialLeftCloud5 = cloud5.offsetLeft;
 
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   let value = window.scrollY;
 
-  text.style.marginTop = value * 0.95 + 'px';
+  text.style.marginTop = value * 0.95 + "px";
 
+  cloud1.style.top = initialTopCloud1 - value * 0.21 + "px"; // Move up
+  cloud1.style.left = initialLeftCloud1 + value * -0.3 + "px"; // Move right
 
-  cloud1.style.top = initialTopCloud1 - value * 0.21 + 'px'; // Move up
-  cloud1.style.left = initialLeftCloud1 + value * -0.3 + 'px'; // Move right
+  cloud2.style.top = initialTopCloud2 - value * 0.25 + "px"; // Move up
+  cloud2.style.left = initialLeftCloud2 + value * 0.8 + "px"; // Move right
 
-  cloud2.style.top = initialTopCloud2 - value * 0.25 + 'px'; // Move up
-  cloud2.style.left = initialLeftCloud2 + value * 0.8 + 'px'; // Move right
+  cloud3.style.top = initialTopCloud3 - value * 0.3 + "px"; // Move up
+  cloud3.style.left = initialLeftCloud3 - value * 0.3 + "px"; // Move right
 
-  cloud3.style.top = initialTopCloud3 - value * 0.3 + 'px'; // Move up
-  cloud3.style.left = initialLeftCloud3 - value * 0.3 + 'px'; // Move right
+  cloud4.style.top = initialTopCloud4 - value * 0.22 + "px"; // Move up
+  cloud4.style.left = initialLeftCloud4 - value * -0.3 + "px"; // Move right
 
-  cloud4.style.top = initialTopCloud4 - value * 0.22 + 'px'; // Move up
-  cloud4.style.left = initialLeftCloud4 - value * -0.3 + 'px'; // Move right
-
-  cloud5.style.top = initialTopCloud5 + value * -0.36 + 'px'; // Move up
-  cloud5.style.left = initialLeftCloud5 + value * -0.2 + 'px'; // Move right
+  cloud5.style.top = initialTopCloud5 + value * -0.36 + "px"; // Move up
+  cloud5.style.left = initialLeftCloud5 + value * -0.2 + "px"; // Move right
 });
-
-
